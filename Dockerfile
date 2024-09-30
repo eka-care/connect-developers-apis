fd46d00954b45279a93491cc81773f58cd564c64
vishal24tuniki <vishal24.tuniki@gmail.com>
2024-09-30 12:55:45 +0530

    Added views and Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM vishal24tuniki/data-slinger-web:v0.0.6
+
+COPY views_connect.py middleware/webapp
---
+++ views_connect.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse
+import json
+import requests
+
+# from .auth import Auth
+
+
+def receive_event(request):
+    if request.method != "POST":
+        return HttpResponse("Hello, world2!")
+
+    data = json.loads(request.body)
+    event_type = data.get("event_type")
+    if event_type != "appointment":
+        return HttpResponse("Hello, world2!")
+    
+    appointment_id = data.get("data", {}).get("appointment_id")
+    if appointment_id is None:
+        return HttpResponse("Hello, world2!")
+    
+    #access_token = Auth().login()
+    access_token = ""
+
+    url = "https://api.eka.care/dr/v1/business/appointments/{}".format(appointment_id)
+    headers = {"auth": access_token}
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        return HttpResponse("Failed appointment API!")
+    
+    appointment_data = response.json()
+    url2 = "https://apirni.metropolisindia.com/rest/prshvtexternalapi/v1/HVT"
+    data = {}
+    response2 = requests.post(url2, json=data)
+    if response2.status_code != 200:
+        return HttpResponse("Failed Metropolis API!")
+
+    return HttpResponse("Hello, world2!")
