89378c8c5c6443716d6dc63ccbfdc0f5967dece8
vishal24tuniki <vishal24.tuniki@gmail.com>
2024-10-07 15:26:08 +0530

    connect APIs

---
+++ connect.py
@@ -0,0 +1,109 @@
+import requests
+from . import constants
+
+
+def register_webhook(scope, endpoint):
+    try:
+        access_token = login()
+    except Exception as e:
+        # TODO: Add logger
+        raise e
+
+    url = "https://api.eka.care/notification/v1/connect/webhook/subscriptions"
+    headers = {
+        constants.CONTENT_TYPE: constants.APPLICATION_JSON,
+        constants.AUTH_HEADER: access_token,
+    }
+
+    data = {
+        "scope": scope,
+        "url": endpoint,
+    }
+
+    try:
+        response = requests.post(url, json=data, headers=headers)
+        response.raise_for_status()  # Raises HTTPError for bad responses (non 2xx)
+        return response.json()
+    except requests.exceptions.HTTPError as http_err:
+        # TODO: Add logger for HTTP errors
+        raise Exception(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        # TODO: Add logger for general errors
+        raise Exception(f"An error occurred: {err}")
+
+
+def list_webhooks():
+    try:
+        access_token = login()
+    except Exception as e:
+        # TODO: Add logger
+        raise e
+
+    url = f"https://api.eka.care/notification/v1/connect/webhook/subscriptions"
+    headers = {
+        constants.AUTH_HEADER: access_token,
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raises HTTPError for bad responses (non 2xx)
+        return response.json()
+    except requests.exceptions.HTTPError as http_err:
+        # TODO: Add logger for HTTP errors
+        raise Exception(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        # TODO: Add logger for general errors
+        raise Exception(f"An error occurred: {err}")
+
+
+def delete_webhook(id):
+    try:
+        access_token = login()
+    except Exception as e:
+        # TODO: Add logger
+        raise e
+
+    url = f"https://api.eka.care/notification/v1/connect/webhook/subscriptions/{id}"
+    headers = {
+        constants.AUTH_HEADER: access_token,
+    }
+
+    try:
+        response = requests.delete(url, headers=headers)
+        response.raise_for_status()  # Raises HTTPError for bad responses (non 2xx)
+        return response.json()
+    except requests.exceptions.HTTPError as http_err:
+        # TODO: Add logger for HTTP errors
+        raise Exception(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        # TODO: Add logger for general errors
+        raise Exception(f"An error occurred: {err}")
+
+
+def update_webhook(id, endpoint):
+    try:
+        access_token = login()
+    except Exception as e:
+        # TODO: Add logger
+        raise e
+
+    url = f"https://api.eka.care/notification/v1/connect/webhook/subscriptions/{id}"
+    headers = {
+        constants.CONTENT_TYPE: constants.APPLICATION_JSON,
+        constants.AUTH_HEADER: access_token,
+    }
+
+    data = {
+        "url": endpoint,
+    }
+
+    try:
+        response = requests.put(url, json=data, headers=headers)
+        response.raise_for_status()  # Raises HTTPError for bad responses (non 2xx)
+        return response.json()
+    except requests.exceptions.HTTPError as http_err:
+        # TODO: Add logger for HTTP errors
+        raise Exception(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        # TODO: Add logger for general errors
+        raise Exception(f"An error occurred: {err}")
